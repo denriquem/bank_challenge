64be727e222561639dc2b79af94b2a59af85c06c
Daniel <demartinezgatell@gmail.com>
2020-01-27 12:16:29 +0000

    withdrwal transaction passing

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -4,5 +4,8 @@ class Transaction
     @money_in = amount
   end
 
+  def withdraw(amount)
+    @money_out = amount
+  end
 
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -2,10 +2,21 @@ require 'transaction'
 
 describe Transaction do
 
+  # beforeEach do
+  #   transaction = Transaction.new
+  # end
+
   describe 'deposit' do
     it 'should return the amount deposited by a user' do
       transaction = Transaction.new
       expect(transaction.deposit(90)).to eq(90)
+    end
+  end
+
+  describe 'withdraw' do
+    it 'should return the amount withdrawn by the user' do
+      transaction = Transaction.new
+    expect(transaction.withdraw(80)).to eq(80)
     end
   end
 
