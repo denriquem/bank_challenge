f6fcc1c96a58bf830ac364ed9bd2259b8ca695f4
Daniel <demartinezgatell@gmail.com>
2020-01-27 16:02:15 +0000

    test: return transaction history as array

--- lib/account.rb
+++ lib/account.rb
@@ -2,9 +2,15 @@
 class Account
   def initialize
     @balance = 0
+    @transactions = []
   end
 
   def balance(transaction_total)
     @balance + transaction_total
   end
+
+  def transaction_history(transaction)
+    @transactions << transaction
+  end
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,19 +2,30 @@ require 'account'
 require 'transaction'
 
 describe Account do
+
+  subject(:account) { described_class.new }
+  subject(:transaction) { Transaction.new }
+
   describe 'check_balance' do
     it 'should return the balance of a user after deposits' do
-      transaction = Transaction.new
       transaction_total = transaction.deposit(90) + transaction.deposit(30)
-      account = Account.new
       expect(account.balance(transaction_total)).to eq(120)
     end
 
     it 'should return balance of user after both deposits and withdrawals' do
-      transaction = Transaction.new
       transaction_total = transaction.deposit(100) + transaction.withdraw(90)
-      account = Account.new
       expect(account.balance(transaction_total)).to eq(10)
+    end
+  end
+
+  describe 'transaction_history' do
+    it 'should return an array of transactions for the account' do
+      account.transaction_history(transaction.deposit(100))
+      account.transaction_history(transaction.deposit(70))
+      account.transaction_history(transaction.withdraw(120))
+      account.transaction_history(transaction.withdraw(10))
+      expected_history = [100, 70, -120, -10, 30]
+      expect(account.transaction_history(transaction.deposit(30))).to eq(expected_history)
     end
   end
 
